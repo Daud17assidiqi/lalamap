5a1c95321ad18a8867669bc5414641e2c888ceb4
daudassidiqi <assidiqidaud@gmail.com>
2024-05-27 17:30:37 +0700

    create custom libraries

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Libraries\CIAuth;
+use App\Libraries\Hash;
+use App\Models\User;
 
 class AuthController extends BaseController
 {
@@ -19,6 +22,8 @@ class AuthController extends BaseController
 
     public function loginHandler()
     {
-        echo 'login handler process...';
+        $fieldType = filter_var($this->request->getVar('login_id'), FILTER_VALIDATE_EMAIL) ? 'email' :  'username';
+
+        echo $fieldType;
     }
 }
