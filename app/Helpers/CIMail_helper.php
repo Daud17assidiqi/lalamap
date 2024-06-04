080fb19b9e711c153736d0a96d7c2f54d380ce57
daudassidiqi <assidiqidaud@gmail.com>
2024-06-04 08:46:20 +0700

    install nesbot & update CIMail

--- app/Helpers/CIMail_helper.php
+++ app/Helpers/CIMail_helper.php
@@ -12,5 +12,26 @@ if (!function_exists('sendEmail')) {
         require 'PHPMailer/src/Exceprion.php';
         require 'PHPMailer/src/PHPMailer.php';
         require 'PHPMailer/src/SMTP.php';
+
+        $mail = new PHPMailer(true);
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = env('EMAIL_HOST');
+        $mail->SMTPAuth = true;
+        $mail->Username = env('EMAIL_USERNAME');
+        $mail->Password = env('EMAIL_PASSWORD');
+        $mail->SMTPSecure = env('EMAIL_ENCRYPTION');
+        $mail->Port = env('EMAIL_PORT');
+        $mail->setFrom($mailConfig['mail_from-email'], $mailConfig['mail_from_name']);
+        $mail->addAddress($mailConfig['mail_recipient_email'], $mailConfig['mail_recipient_namae']);
+        $mail->isHTML(true);
+        $mail->Subject = $mailConfig['mail_subject'];
+        $mail->Body = $mailConfig['mail_body'];
+        if ($mail->send()) {
+            # code...
+            return true;
+        } else {
+            return false;
+        }
     }
 }
